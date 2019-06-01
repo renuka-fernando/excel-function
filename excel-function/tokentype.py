4aaab17298774c3e25181283cbbbcbd032c773c5
Renuka Fernando <renuka.piyumal@gmail.com>
2019-06-01 21:04:23 +0530

    add NumberTokenType class

--- excel-function/tokentype.py
+++ excel-function/tokentype.py
@@ -16,7 +16,17 @@ class FieldTokenType(TokenType):
         i = 1
         while query[start_index + i] != '"':
             i += 1
-        return (query[start_index + 1:start_index + i], start_index + i + 1)
+        return query[start_index + 1:start_index + i], start_index + i + 1
 
 
-print(FieldTokenType().get_token_value('15 * "Hello" + 12', 5))
+class NumberTokenType(TokenType):
+    value = "Number"
+
+    def get_token_value(self, query, start_index):
+        i = 1
+        while query[start_index + i] in '0123456789.':
+            i += 1
+        return query[start_index:start_index + i], start_index + i
+
+
+print(NumberTokenType().get_token_value('455 / 15.2 * "Hello" + 12', 6))
