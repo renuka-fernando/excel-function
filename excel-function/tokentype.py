3cebf4c0dc4bba3a4f879037f5ac5c5d5adc1d41
Renuka Fernando <renuka.piyumal@gmail.com>
2019-06-01 21:26:54 +0530

    add get_token_type method to infer type of a token

--- excel-function/tokentype.py
+++ excel-function/tokentype.py
@@ -1,3 +1,6 @@
+token_types = ['FieldTokenType', 'NumberTokenType']
+
+
 class TokenType:
     value = None
 
@@ -5,28 +8,49 @@ class TokenType:
     def get_type():
         return 1
 
-    def get_token_value(self, query, start_index):
+    @staticmethod
+    def get_token_type(start_of_query):
+        for token_type in token_types:
+            if eval('%s.is_this_type(start_of_query)' % token_type):
+                return NumberTokenType
+
+    @staticmethod
+    def get_token_value(query, start_index):
+        return NotImplementedError()
+
+    @staticmethod
+    def is_this_type(start_of_query):
         return NotImplementedError()
 
 
 class FieldTokenType(TokenType):
     value = "Field"
 
-    def get_token_value(self, query, start_index):
+    @staticmethod
+    def get_token_value(query, start_index):
         i = 1
         while query[start_index + i] != '"':
             i += 1
         return query[start_index + 1:start_index + i], start_index + i + 1
 
+    @staticmethod
+    def is_this_type(start_of_query):
+        return False
+
 
 class NumberTokenType(TokenType):
     value = "Number"
 
-    def get_token_value(self, query, start_index):
+    @staticmethod
+    def get_token_value(query, start_index):
         i = 1
         while query[start_index + i] in '0123456789.':
             i += 1
         return query[start_index:start_index + i], start_index + i
 
+    @staticmethod
+    def is_this_type(start_of_query):
+        return True
 
-print(NumberTokenType().get_token_value('455 / 15.2 * "Hello" + 12', 6))
+
+print(TokenType.get_token_type('').get_token_value('455 / 15.2 * "Hello" + 12', 6))
