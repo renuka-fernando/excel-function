0f136acb9c6b96ba9fb64a30b84107cd9f10642a
Renuka Fernando <renuka.piyumal@gmail.com>
2019-06-01 22:18:06 +0530

    ignore spaces in the query

--- excel-function/tokentype.py
+++ excel-function/tokentype.py
@@ -1,4 +1,4 @@
-token_types = ['FieldTokenType', 'NumberTokenType']
+token_types = ['IgnoreTokenType', 'FieldTokenType', 'NumberTokenType']
 
 
 class TokenType:
@@ -24,19 +24,36 @@ class TokenType:
         return NotImplementedError()
 
 
-class FieldTokenType(TokenType):
-    value = "Field"
+class IgnoreTokenType(TokenType):
+    value = "Ignore"
+    symbols = ' '
 
     @staticmethod
     def get_token_value(query, start_index):
         i = 1
-        while query[start_index + i] != '"':
+        while query[start_index + i] in IgnoreTokenType.symbols:
+            i += 1
+        return None, start_index + i
+
+    @staticmethod
+    def is_this_type(start_of_query):
+        return start_of_query[0] in IgnoreTokenType.symbols
+
+
+class FieldTokenType(TokenType):
+    value = "Field"
+    symbols = '"'
+
+    @staticmethod
+    def get_token_value(query, start_index):
+        i = 1
+        while query[start_index + i] != FieldTokenType.symbols:
             i += 1
         return query[start_index + 1:start_index + i], start_index + i + 1
 
     @staticmethod
     def is_this_type(start_of_query):
-        return start_of_query[0] == '"'
+        return start_of_query[0] == FieldTokenType.symbols
 
 
 class NumberTokenType(TokenType):
@@ -62,11 +79,13 @@ def tokenize(query):
     while start_index < len(query):
         token_type = TokenType.get_token_type(query[start_index:])
         value, start_index = token_type.get_token_value(query, start_index)
-        tokens.append(value)
+
+        if value != None:
+            tokens.append(value)
 
     return tokens
 
 
 # query = '455 / 15.2 * "Hello" + 12'
-query = '455"Hello"78.24"Renuka""Fernando"'
+query = '  455  "Hello"  78.24  "Renuka"  "Fernando"'
 print(tokenize(query))
