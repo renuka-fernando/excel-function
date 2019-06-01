065a3d018ff1949ece48122836c7ee6162c5bc16
Renuka Fernando <renuka.piyumal@gmail.com>
2019-06-01 21:50:31 +0530

    add symbols for token type with is_this_type method

--- excel-function/tokentype.py
+++ excel-function/tokentype.py
@@ -3,6 +3,7 @@ token_types = ['FieldTokenType', 'NumberTokenType']
 
 class TokenType:
     value = None
+    symbols = None
 
     @staticmethod
     def get_type():
@@ -12,7 +13,7 @@ class TokenType:
     def get_token_type(start_of_query):
         for token_type in token_types:
             if eval('%s.is_this_type(start_of_query)' % token_type):
-                return NumberTokenType
+                return eval(token_type)
 
     @staticmethod
     def get_token_value(query, start_index):
@@ -35,22 +36,24 @@ class FieldTokenType(TokenType):
 
     @staticmethod
     def is_this_type(start_of_query):
-        return False
+        return start_of_query[0] == '"'
 
 
 class NumberTokenType(TokenType):
     value = "Number"
+    symbols = '0123456789.'
 
     @staticmethod
     def get_token_value(query, start_index):
         i = 1
-        while query[start_index + i] in '0123456789.':
+        while query[start_index + i] in NumberTokenType.symbols:
             i += 1
         return query[start_index:start_index + i], start_index + i
 
     @staticmethod
     def is_this_type(start_of_query):
-        return True
+        return start_of_query[0] in NumberTokenType.symbols
 
 
-print(TokenType.get_token_type('').get_token_value('455 / 15.2 * "Hello" + 12', 6))
+query = '455 / 15.2 * "Hello" + 12'
+print(TokenType.get_token_type(query[6]).get_token_value(query, 6))
